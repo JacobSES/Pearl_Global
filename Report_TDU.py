d0f417ea7121440814e1adb2757275f91509a203
JacobSES <jacob@electricalses.com.au>
2022-07-25 14:16:24 +1000

    .

--- Report_TDU.py
+++ Report_TDU.py
@@ -24,7 +24,7 @@ class TDU:
         self.conn = self.init_connection()
 
     def init_connection(self):
-        engine = "DRIVER={ODBC+Driver+17+for+SQL+Server};SERVER=" + st.secrets["server"] + ";DATABASE=" + st.secrets["database"] + ";UID=" + st.secrets["username"] + ";PWD=" + st.secrets["password"]
+        engine = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=" + st.secrets["server"] + ";DATABASE=" + st.secrets["database"] + ";UID=" + st.secrets["username"] + ";PWD=" + st.secrets["password"]
         print(engine)
         return pyodbc.connect(engine)
         
