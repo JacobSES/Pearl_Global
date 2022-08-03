453d0de82e5dfb5f8faaf168b045148bf9a047f8
JacobSES <jacob@electricalses.com.au>
2022-08-03 14:15:31 +1000

    Update Report_Dashboard.py

--- Report_Dashboard.py
+++ Report_Dashboard.py
@@ -18,7 +18,7 @@ class DASHBOARD:
     def __init__(self, TDU_LIST):
         ##page configuration
         # with st.echo(code_location='below'):
-            self.today = (str)(datetime.date.today())
+            self.today = (datetime.date.today())
             self.yesterday = (str)(datetime.date.today() - datetime.timedelta(days = 1))
             self.start_day = (str)(datetime.date.today() - datetime.timedelta(days = 7))
             self.last_week_start = (str)(datetime.date.today() - datetime.timedelta(days = 14))
