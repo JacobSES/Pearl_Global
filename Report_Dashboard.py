a4f0272f0aa9d53c24b9ccd06dfef6ff2e6b2f49
JacobSES <jacob@electricalses.com.au>
2022-08-03 13:44:25 +1000

    Update Report_Dashboard.py

--- Report_Dashboard.py
+++ Report_Dashboard.py
@@ -57,7 +57,7 @@ class DASHBOARD:
             m1,m2,m3,m4,m5 = st.columns((1,1,1,1,1))
             self.weekly_performance_df = self.read_weekly_performance_df(self.start_day, self.today)
             self.lastWeek_performance_df = self.read_weekly_performance_df(self.last_week_start, self.start_day)
-            st.write(self.weekly_performance_df)
+            st.write(self.weekly_performance_df[tdu_index].head())
             # Today_data = self.weekly_performance_df[tdu_index].loc[self.today]
             # Yesterday_data = self.weekly_performance_df[tdu_index].loc[self.yesterday]
 
